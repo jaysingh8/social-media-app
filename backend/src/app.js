72811be6e9eb4a0eaf13507a1f37f1b2080f0272
Jay Singh <jaysinghrajput265@gmail.com>
2026-04-17 01:13:59 +0530

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ app.use(cookieParser())
 app.use(cors(
     {
         credentials:true,
-        origin:"http://localhost:5173"
+        origin:"https://social-media-app-1-gl3j.onrender.com"
     }
 ))
 
