b68c0735a3befc93c3838bd3ce600adf27ee0fc7
Jay Singh <jaysinghrajput265@gmail.com>
2026-04-17 01:20:26 +0530

    update

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { base } from '../Backend/src/models/user.model';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+   
+  ],
+  base:"/"
 })
+
