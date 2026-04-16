8ed5ee0935ccd28d6f395910a7d995b2c06c06d1
Jay Singh <jaysinghrajput265@gmail.com>
2026-04-17 01:24:37 +0530

    fixed frontend build error

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { base } from '../Backend/src/models/user.model';
+
 
 // https://vite.dev/config/
 export default defineConfig({
